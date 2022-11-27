05be2fab2daa24502ad1e2a83c5eea67619ff0b4
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-11-27 18:48:21 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
+  return (
+    <div className="App">
+      <Counter></Counter>
+    </div>
+  );
+}
+function Counter(){
+  const [roll,setRoll] = useState(33);
+  const increaseCount = () => setRoll(roll +1);
+  const decreaseCount = () =>{
+    if(roll > 0)
+    setRoll(roll - 1);
+  } 
+  // const increaseCount = () =>{
+  //   const currentRoll = roll +1;
+  //   setRoll(currentRoll);
+  return (
+    <div>
+      <h1>Roll :{roll} </h1>
+      <button onClick={increaseCount}>increase</button>
+      <button onClick={decreaseCount}>decrease</button>
+    </div>
+    
+    )
+}
+
+
+{/*  (normal example video: 6...step..1 uporer theke)
+const products = [
     {name: 'laptop',price:'10001',brand: 'japani'},
     {name: 'mobile',price:'9001',brand: 'asian'},
     {name: 'watch',price:'100',brand: 'sona'},
     {name: 'torch',price:'2001',brand: 'korean'}
   ]
-  return (
-    <div className="App">
+/*}
+{/* (normal example video: 6...step..2 uporer theke)
       {
         products.map(product => <Product name = {product.name} price='2000'></Product>)
       }
 {/*     
       <Product name ="laptoppue" price="11000" ></Product>
       <Product name ="mobile" price="2100" ></Product>
-      <Product name ="watch" price="1200" ></Product> */}
-    </div>
-  );
-}
+      <Product name ="watch" price="1200" ></Product> 
+       */}
+
+{/*    (normal example video: 6...step..3 uporer theke)
 function Product(props) {
   return (
     <div className="product">
@@ -28,5 +57,8 @@ function Product(props) {
     </div>
   );
 }
+
+* */}
+
 
 export default App;
